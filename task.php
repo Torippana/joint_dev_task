54d39fb65ff948f4d1f47b9a509d1a8b78e3c1b5
Torippana <17624328+Torippana@users.noreply.github.com>
2020-10-09 01:04:02 +0900

    done q1 to q8

--- task.php
+++ task.php
@@ -1,62 +1,64 @@
 # 課題の回答は このファイル をご利用下さい。
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
-
+<?php
 print("#####q1#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
-
-  # 以下に回答を記載
-
+array_push($names, '斎藤');
+print_r($names);
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
 $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
-
-  # 以下に回答を記載
-
+$array = array_merge($array1,$array2);
+print_r($array);
 echo PHP_EOL;
 
 print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
-
-  # 以下に回答を記載
-
+$count = 0;
+foreach($numbers as $number) {
+  if ($number === 3) {
+    $count++;
+  }
+}
+echo $count;
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
-
-  # 以下に回答を記載
-
+$sports = array_filter($sports);
+print_r($sports);
 echo PHP_EOL;
 
 print("#####q5#####".PHP_EOL);
 $array1 = [];
 $array2 = [1, 5, 8, 10];
-
-  # 以下に回答を記載
-
+var_export(empty($array1));
+var_export(empty($array2));
 echo PHP_EOL;
 
 print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
-
-  # 以下に回答を記載
-
+$numbers2 = [];
+foreach ($numbers1 as $number) {
+  $numbers2[] = $number * 10;
+}
+print_r($numbers2);
 echo PHP_EOL;
 
 print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
-  # 以下に回答を記載
-
+foreach ($array as $index => $value) {
+    $array[$index] = (int) $value;
+}
+print_r($array);
 echo PHP_EOL;
 
 print("#####q8#####".PHP_EOL);
 $programming_languages = ["php","ruby","python","javascript"];
-
-  # 以下に回答を記載
-
+$upper_case_programming_languages = array_map('strtoupper', $programming_languages);
   # 以下は変更しないで下さい
 print_r($programming_languages);
 echo PHP_EOL;
@@ -183,9 +185,9 @@ class Human
 
 class Zoo
 {
-    
+
   # コードを追加
-    
+
 }
 
 $zoo = new Zoo("旭山動物園",[ "infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
